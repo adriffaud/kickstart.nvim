b8432a45762570669cf29384a04181b1ab96150e
Adrien Driffaud <adrien@driffaud.fr>
2024-08-01 09:30:01 +0200

    change theme

--- init.lua
+++ init.lua
@@ -803,31 +803,32 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-storm'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000,
+  --   init = function()
+  --     vim.cmd.colorscheme 'tokyonight-storm'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
   {
-    'maxmx03/solarized.nvim',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
     init = function()
-      -- vim.opt.background = 'light'
-      -- vim.cmd.colorscheme 'solarized'
+      vim.cmd.colorscheme 'catppuccin-macchiato'
     end,
   },
+  -- {
+  --   'maxmx03/solarized.nvim',
+  --   priority = 1000,
+  --   init = function()
+  --     -- vim.opt.background = 'light'
+  --     -- vim.cmd.colorscheme 'solarized'
+  --   end,
+  -- },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
